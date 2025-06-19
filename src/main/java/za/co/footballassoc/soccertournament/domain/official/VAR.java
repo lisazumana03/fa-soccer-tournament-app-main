c8b0062d8443c67f5545c932ad870a333a423841
lisazumana03 <zumanalisa@gmail.com>
2025-06-19 16:44:20 +0200

    file incoming

--- src/main/java/za/co/footballassoc/soccertournament/domain/official/VAR.java
+++ src/main/java/za/co/footballassoc/soccertournament/domain/official/VAR.java
@@ -1,4 +1,38 @@
 package za.co.footballassoc.soccertournament.domain.official;
 
-public class VAR extends Official {
+import jakarta.persistence.Entity;
+
+import java.io.Serializable;
+
+@Entity
+public class VAR extends Official implements Serializable {
+    private int techExperience;
+
+    public VAR() {}
+
+    private VAR(Builder builder) {
+        this.techExperience = builder.techExperience;
+    }
+
+    public int getTechExperience() {
+        return techExperience;
+    }
+
+    @Override
+    public String toString() {
+        return "VAR{" +
+                "techExperience=" + techExperience +
+                ", officialName=" + officialName +
+                ", officialNationality='" + officialNationality + '\'' +
+                ", officialDateOfBirth=" + officialDateOfBirth +
+                ", officialLocationOfBirth=" + officialLocationOfBirth +
+                ", officialOrganisation='" + officialOrganisation + '\'' +
+                ", match=" + match +
+                '}';
+    }
+
+    public static class Builder{
+        private int techExperience;
+    }
+
 }
