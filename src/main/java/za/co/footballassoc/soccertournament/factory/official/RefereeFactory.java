00e779f379bba7a945dcaeb41b6a546f1c843f6b
lisazumana03 <zumanalisa@gmail.com>
2025-06-20 22:21:17 +0200

    file incoming

--- src/main/java/za/co/footballassoc/soccertournament/factory/official/RefereeFactory.java
+++ src/main/java/za/co/footballassoc/soccertournament/factory/official/RefereeFactory.java
@@ -1,4 +1,11 @@
 package za.co.footballassoc.soccertournament.factory.official;
 
+import za.co.footballassoc.soccertournament.domain.Name;
+import za.co.footballassoc.soccertournament.domain.official.Referee;
+
+import java.time.LocalDate;
+
 public class RefereeFactory {
+    public static Referee createReferee(String officialId, Name officialName, String officialNationality,
+                                        LocalDate officialDateOfBirth, ) {}
 }
