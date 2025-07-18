0ecd82374006fc434973a68085df6ee6d164d762
lisazumana03 <zumanalisa@gmail.com>
2025-07-19 00:09:53 +0200

    file incoming

---
+++ src/main/java/za/co/footballassoc/soccertournament/controller/official/VARController.java
@@ -0,0 +1,15 @@
+package za.co.footballassoc.soccertournament.controller.official;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import za.co.footballassoc.soccertournament.service.official.impl.VARService;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/var")
+public class VARController {
+    @Autowired
+    private VARService varService;
+}
---
+++ src/main/java/za/co/footballassoc/soccertournament/controller/team/OwnerController.java
@@ -0,0 +1,11 @@
+package za.co.footballassoc.soccertournament.controller.team;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/owner")
+public class OwnerController {
+}
