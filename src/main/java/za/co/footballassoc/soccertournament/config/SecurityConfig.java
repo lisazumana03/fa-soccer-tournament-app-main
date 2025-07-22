4433034f4fc660b2f81302cae48f89b1c3e1ceec
lisazumana03 <zumanalisa@gmail.com>
2025-07-22 16:23:53 +0200

    file incoming

--- src/main/java/za/co/footballassoc/soccertournament/config/SecurityConfig.java
+++ src/main/java/za/co/footballassoc/soccertournament/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package za.co.footballassoc.soccertournament.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
+@EnableWebSecurity
 public class SecurityConfig {
 }
