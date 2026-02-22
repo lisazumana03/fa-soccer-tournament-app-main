49eaafed1d02e0aed42f7abbed98344bcd98ff89
lisazumana03 <zumanalisa@gmail.com>
2026-02-22 12:34:36 +0200

    file incoming

--- src/main/java/za/co/footballassoc/soccertournament/controller/match/VenueController.java
+++ src/main/java/za/co/footballassoc/soccertournament/controller/match/VenueController.java
@@ -1,11 +1,9 @@
 package za.co.footballassoc.soccertournament.controller.match;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import za.co.footballassoc.soccertournament.domain.match.Venue;
 import za.co.footballassoc.soccertournament.service.match.impl.VenueService;
 
@@ -15,8 +13,18 @@ public class VenueController {
     @Autowired
     private VenueService venueService;
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<Venue> createVenue(@RequestBody Venue venue) {
         return ResponseEntity.ok(venueService.create(venue));
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> deleteVenue(@RequestBody String venueID) {
+        try {
+            venueService.delete(venueID);
+            return ResponseEntity.noContent().build(); // 204
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build(); // 404
+        }
     }
 }
--- src/main/java/za/co/footballassoc/soccertournament/domain/match/MatchEvent.java
+++ src/main/java/za/co/footballassoc/soccertournament/domain/match/MatchEvent.java
@@ -13,7 +13,7 @@ public class MatchEvent implements Serializable {
     private String eventID;
     @ManyToOne
     private Match match;
-    private int minute; // Minute of the match (e.g., 23, 45+2, 90+5)
+    private int minute; // Minute of the match (e.g., 0, 45+, 45, 90 (Regular time), 90, 105+, 105, 120+)
 
     @Enumerated(EnumType.STRING)
     private EventType eventType;
@@ -33,5 +33,197 @@ public class MatchEvent implements Serializable {
     private Venue venueSelection;
     private String assistantRefereeDecision;
     private boolean isInjuryRelated;
+
+    public MatchEvent() {}
+
+    private MatchEvent(Builder builder) {
+        this.eventID = builder.eventID;
+        this.match = builder.match;
+        this.minute = builder.minute;
+        this.eventType = builder.eventType;
+        this.playerInvolved = builder.playerInvolved;
+        this.team = builder.team;
+        this.eventDescription = builder.eventDescription;
+        this.isVARReviewed = builder.isVARReviewed;
+        this.isCancelledByVAR = builder.isCancelledByVAR;
+        this.additionalInfo = builder.additionalInfo;
+        this.venueSelection = builder.venueSelection;
+        this.assistantRefereeDecision = builder.assistantRefereeDecision;
+        this.isInjuryRelated = builder.isInjuryRelated;
+    }
+
+    public String getEventID() {
+        return eventID;
+    }
+
+    public Match getMatch() {
+        return match;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public Player getPlayerInvolved() {
+        return playerInvolved;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public String getEventDescription() {
+        return eventDescription;
+    }
+
+    public boolean isVARReviewed() {
+        return isVARReviewed;
+    }
+
+    public boolean isCancelledByVAR() {
+        return isCancelledByVAR;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public Venue getVenueSelection() {
+        return venueSelection;
+    }
+
+    public String getAssistantRefereeDecision() {
+        return assistantRefereeDecision;
+    }
+
+    public boolean isInjuryRelated() {
+        return isInjuryRelated;
+    }
+
+    @Override
+    public String toString() {
+        return "MatchEvent{" +
+                "eventID='" + eventID + '\'' +
+                ", match=" + match +
+                ", minute=" + minute +
+                ", eventType=" + eventType +
+                ", playerInvolved=" + playerInvolved +
+                ", team=" + team +
+                ", eventDescription='" + eventDescription + '\'' +
+                ", isVARReviewed=" + isVARReviewed +
+                ", isCancelledByVAR=" + isCancelledByVAR +
+                ", additionalInfo='" + additionalInfo + '\'' +
+                ", venueSelection=" + venueSelection +
+                ", assistantRefereeDecision='" + assistantRefereeDecision + '\'' +
+                ", isInjuryRelated=" + isInjuryRelated +
+                '}';
+    }
+
+    public static class Builder{
+        private String eventID;
+        private Match match;
+        private int minute;
+        private EventType eventType;
+        private Player playerInvolved;
+        private Team team;
+        private String eventDescription;
+        private boolean isVARReviewed;
+        private boolean isCancelledByVAR;
+        private String additionalInfo;
+        private Venue venueSelection;
+        private String assistantRefereeDecision;
+        private boolean isInjuryRelated;
+
+        public Builder setEventID(String eventID) {
+            this.eventID = eventID;
+            return this;
+        }
+
+        public Builder setMatch(Match match) {
+            this.match = match;
+            return this;
+        }
+
+        public Builder setMinute(int minute) {
+            this.minute = minute;
+            return this;
+        }
+
+        public Builder setEventType(EventType eventType) {
+            this.eventType = eventType;
+            return this;
+        }
+
+        public Builder setPlayerInvolved(Player playerInvolved) {
+            this.playerInvolved = playerInvolved;
+            return this;
+        }
+
+        public Builder setTeam(Team team) {
+            this.team = team;
+            return this;
+        }
+
+        public Builder setEventDescription(String eventDescription) {
+            this.eventDescription = eventDescription;
+            return this;
+        }
+
+        public Builder setIsVARReviewed(boolean isVARReviewed) {
+            this.isVARReviewed = isVARReviewed;
+            return this;
+        }
+
+        public Builder setIsCancelledByVAR(boolean isCancelledByVAR) {
+            this.isCancelledByVAR = isCancelledByVAR;
+            return this;
+        }
+
+        public Builder setAdditionalInfo(String additionalInfo) {
+            this.additionalInfo = additionalInfo;
+            return this;
+        }
+
+        public Builder setVenueSelection(Venue venueSelection) {
+            this.venueSelection = venueSelection;
+            return this;
+        }
+
+        public Builder setAssistantRefereeDecision(String assistantRefereeDecision) {
+            this.assistantRefereeDecision = assistantRefereeDecision;
+            return this;
+        }
+
+        public Builder setInjuryRelated(boolean isInjuryRelated) {
+            this.isInjuryRelated = isInjuryRelated;
+            return this;
+        }
+
+        public Builder copy(MatchEvent event){
+            this.eventID = event.eventID;
+            this.match = event.match;
+            this.minute = event.minute;
+            this.eventType = event.eventType;
+            this.playerInvolved = event.playerInvolved;
+            this.team = event.team;
+            this.eventDescription = event.eventDescription;
+            this.isVARReviewed = event.isVARReviewed;
+            this.isCancelledByVAR = event.isCancelledByVAR;
+            this.additionalInfo = event.additionalInfo;
+            this.venueSelection = event.venueSelection;
+            this.assistantRefereeDecision = event.assistantRefereeDecision;
+            this.isInjuryRelated = event.isInjuryRelated;
+            return this;
+        }
+
+        public MatchEvent build() {
+            return new MatchEvent(this);
+        }
+
+    }
 
 }
