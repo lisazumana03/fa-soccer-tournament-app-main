2e655bb402b47668adc9b70a3751972d36ebe602
lisazumana03 <zumanalisa@gmail.com>
2026-02-22 12:57:17 +0200

    file incoming

--- src/main/java/za/co/footballassoc/soccertournament/factory/match/MatchEventFactory.java
+++ src/main/java/za/co/footballassoc/soccertournament/factory/match/MatchEventFactory.java
@@ -1,7 +1,32 @@
 package za.co.footballassoc.soccertournament.factory.match;
 
+import za.co.footballassoc.soccertournament.domain.match.EventType;
+import za.co.footballassoc.soccertournament.domain.match.Match;
 import za.co.footballassoc.soccertournament.domain.match.MatchEvent;
+import za.co.footballassoc.soccertournament.domain.match.Venue;
+import za.co.footballassoc.soccertournament.domain.team.Player;
+import za.co.footballassoc.soccertournament.domain.team.Team;
 
 public class MatchEventFactory {
-    public static MatchEvent addMatchEvent()
+    public static MatchEvent addMatchEvent(String eventID, Match match, int minute,
+                                           EventType eventType, Player playerInvolved, Team teamInvolved,
+                                           String eventDescription, boolean isVARReviewed, boolean isCancelledByVAR,
+                                           String additionalInfo, Venue venueSelection, String assistantRefereeDecision,
+                                           boolean injuryRelated) {
+        return new MatchEvent.Builder()
+                .setEventID(eventID)
+                .setMatch(match)
+                .setMinute(minute)
+                .setEventType(eventType)
+                .setPlayerInvolved(playerInvolved)
+                .setTeam(teamInvolved)
+                .setEventDescription(eventDescription)
+                .setIsVARReviewed(isVARReviewed)
+                .setIsCancelledByVAR(isCancelledByVAR)
+                .setAdditionalInfo(additionalInfo)
+                .setVenueSelection(venueSelection)
+                .setAssistantRefereeDecision(assistantRefereeDecision)
+                .setInjuryRelated(injuryRelated)
+                .build();
+    }
 }
---
+++ src/test/java/za/co/footballassoc/soccertournament/factory/match/MatchEventFactoryTest.java
@@ -0,0 +1,12 @@
+package za.co.footballassoc.soccertournament.factory.match;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatchEventFactoryTest {
+
+    @Test
+    void addMatchEvent() {
+    }
+}
