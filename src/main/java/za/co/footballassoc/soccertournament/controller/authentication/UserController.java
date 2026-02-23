bed5613cd99b192b2fba6a3bc719e4381ce97d8b
Lisa Zumana <zumanalisa@gmail.com>
2026-02-23 10:44:14 +0200

    file incoming

--- src/main/java/za/co/footballassoc/soccertournament/controller/authentication/UserController.java
+++ src/main/java/za/co/footballassoc/soccertournament/controller/authentication/UserController.java
@@ -1,8 +1,10 @@
+package za.co.footballassoc.soccertournament.controller.authentication;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController{
-    
+
 }
