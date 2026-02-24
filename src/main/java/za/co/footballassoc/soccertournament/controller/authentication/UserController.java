8ab3ff028767a7c99f33b75637eea5a7243d844f
Lisa Zumana <zumanalisa@gmail.com>
2026-02-24 10:23:38 +0200

    file incoming

--- src/main/java/za/co/footballassoc/soccertournament/controller/authentication/UserController.java
+++ src/main/java/za/co/footballassoc/soccertournament/controller/authentication/UserController.java
@@ -1,10 +1,56 @@
 package za.co.footballassoc.soccertournament.controller.authentication;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/user")
-public class UserController{
+import za.co.footballassoc.soccertournament.domain.Name;
+import za.co.footballassoc.soccertournament.domain.authentication.Role;
+import za.co.footballassoc.soccertournament.domain.authentication.User;
+import za.co.footballassoc.soccertournament.service.authentication.impl.UserService;
 
+@RestController
+//@CrossOrigin(origins = "*")
+@RequestMapping("/api/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody Map<String, String> body) {
+        Name name = new Name.Builder()
+                .setFirstName(body.get("firstName"))
+                .setMiddleName(body.get("middleName"))
+                .setLastName(body.get("lastName"))
+                .build();
+
+        Role role = Role.valueOf(body.get("role").toUpperCase());
+
+        User user = userService.registerUser(
+                name,
+                body.get("userName"),
+                body.get("email"),
+                body.get("password"),
+                body.get("phoneNumber"),
+                role);
+        return ResponseEntity.ok("Registered user: " + user.getUserName());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody Map<String, String> body) {
+        String username = body.get("userName");
+        String password = body.get("password");
+        String result = userService.authenticateUser(username, password);
+        if (result != null && !result.isEmpty()) {
+            return ResponseEntity.ok("Login successful for user: " + username);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+    }
 }
--- src/main/java/za/co/footballassoc/soccertournament/service/authentication/IUserService.java
+++ src/main/java/za/co/footballassoc/soccertournament/service/authentication/IUserService.java
@@ -1,5 +1,10 @@
 package za.co.footballassoc.soccertournament.service.authentication;
 
+import za.co.footballassoc.soccertournament.domain.Name;
+import za.co.footballassoc.soccertournament.domain.authentication.Role;
+import za.co.footballassoc.soccertournament.domain.authentication.User;
+
 public interface IUserService {
-    
+    User registerUser(Name name, String username, String email, String password, String phoneNumber, Role role);
+    String authenticateUser(String username, String password);
 }
